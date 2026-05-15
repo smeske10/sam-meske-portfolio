d61045eb1acbf531c055d98a7226bb610aff0df4
smeske10 <35307525+smeske10@users.noreply.github.com>
2026-05-15 14:20:31 -0400

    Theme: stop forcing dark class in root layout

--- vision-venture-site/src/app/layout.tsx
+++ vision-venture-site/src/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <script
